3aa28563272afa01dda79df4d774f4c8961ab85c
Jon Cram <jon@webignition.net>
2019-04-01 17:02:27 +0100

    Remove Configuration

--- src/LinkChecker.php
+++ src/LinkChecker.php
@@ -9,33 +9,18 @@ use webignition\UrlHealthChecker\LinkState;
 
 class LinkChecker
 {
-    const HTTP_STATUS_CODE_OK = 200;
-    const HTTP_STATUS_CODE_METHOD_NOT_ALLOWED = 405;
-    const HTTP_STATUS_CODE_NOT_IMPLEMENTED = 501;
-
-    const CURL_MALFORMED_URL_CODE = 3;
-    const CURL_MALFORMED_URL_MESSAGE = 'The URL was not properly formatted.';
-
-    const BAD_REQUEST_LIMIT = 3;
-
     /**
      * @var array
      */
     private $urlToLinkStateMap = [];
 
     /**
-     * @var Configuration
-     */
-    private $configuration;
-
-    /**
      * @var UrlHealthChecker
      */
     private $urlHealthChecker = null;
 
-    public function __construct(Configuration $configuration, UrlHealthChecker $urlHealthChecker)
+    public function __construct(UrlHealthChecker $urlHealthChecker)
     {
-        $this->configuration = $configuration;
         $this->urlHealthChecker = $urlHealthChecker;
     }
 
@@ -60,17 +45,8 @@ class LinkChecker
 
     private function createComparisonUrl(string $url): string
     {
-        if (false === $this->configuration->getIgnoreFragmentInUrlComparison()) {
-            return $url;
-        }
-
         $uri = new Uri($url);
         $uri = Normalizer::normalize($uri);
-
-        if (empty(trim($uri->getFragment()))) {
-            return $url;
-        }
-
         $uri = $uri->withFragment('');
 
         return (string) $uri;
